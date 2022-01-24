4d55e605e0f41f627809bec2941c86f2c9cf232b
bharat khairnar <bharat.khairnar@bacancy.com>
2022-01-24 11:53:10 +0530

    changes in files

---
+++ src/components/dashboard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <div class="header">
+    <a href="#" class="logo">Dashboard</a>
+    <div class="header-right">
+      <a href="#">Products</a>
+      <a href="#">Sign out</a>
+    </div>
+  </div>
+    <Product />
+  </div>
+</template>
+
+<script>
+import Product from '../components/Product.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Product
+  }
+}
+</script>
+<style scoped>
+
+.header {
+  overflow: hidden;
+  background-color: #a7bfe2;
+  padding: 20px 10px;
+  position:fixed;
+  top:0;
+  width:100%;;
+}
+
+.header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  
+  .header-right {
+    float: none;
+  }
+}
+.card.container{
+  margin-top: 150px;
+}
+</style>
