b5fb5747ce9c139a2395f124905cb9a3926c2567
Ankit Kabra <ankitswonders@gmail.com>
2022-01-27 12:12:53 +0530

    Homepage UI fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,63 @@
 <template>
   <Main>
     <template #iteminbread>
-       Main
+      Main
     </template>
     <template #content>
       <h1>
         All Products
       </h1>
-
+      <hr>
       <div v-if="!loading" class="row">
-          <div v-for="product in products" :key="product.id" class="mb-3 col-md-4">
-           <div class="h-100 card">
-            <img :src="product.image" class="card-img-custom card-img-top" alt="...">
-            <div class="card-body">
+        <div v-for="product in products" :key="product.id" class="mb-3 col-md-4">
+          <div class="h-100 card">
+            <div class="border-primay bg-primary card-header">
               <h5 class="card-title">
-                {{product.title}}
+                <a href="#" class="text-light">
+                  {{product.title}}
+                </a>
               </h5>
+            </div>
+            <a href="#">
+              <img :src="product.image" class="card-img-custom card-img-top" alt="...">
+            </a>
+            <div class="card-body">
+              
               <p class="card-text">
-                
-                {{ product.description.length > 150 ? product.description.substring(0, 150) + '...' : product.description }}
-                
-              </p>
 
-              <h4>
-                ${{product.price}}
-              </h4>
-              <hr>
-              <a @click.prevent="addtocart(1,product.price,'L','White',product.title)" class="btn btn-primary">
+                {{ product.description.length > 100 ? product.description.substring(0, 100) + '...' : product.description }}
+
+              </p>
+        
+            </div>
+            <div class="card-footer">
+              <a @click.prevent="addtocart(1,product.price,'L','White',product.title)" class="btn btn-success btn-md">
                 <i class="fa fa-cart-plus"></i> ADD TO CART
               </a>
+
+              <h4 class="text-danger pull-right">
+                 ${{product.price.toFixed(2)}}
+              </h4>
             </div>
           </div>
-          </div>
+        </div>
       </div>
       <div v-else>
-         <b-row>
-           <b-col class="mb-3" v-for="(i,index) in 6" :key="index" cols="12" md="4">
+        <b-row>
+          <b-col class="mb-3" v-for="(i,index) in 6" :key="index" cols="12" md="4">
             <b-card class="h-100" no-body img-top>
               <b-skeleton-img card-img="top" aspect="3:1"></b-skeleton-img>
               <b-card-body>
-                  <b-skeleton animation="fade" width="85%"></b-skeleton>
+                <b-skeleton animation="fade" width="85%"></b-skeleton>
 
-                  <b-skeleton animation="throb" width="85%"></b-skeleton>
-                  <b-skeleton animation="throb" width="55%"></b-skeleton>
+                <b-skeleton animation="throb" width="85%"></b-skeleton>
+                <b-skeleton animation="throb" width="55%"></b-skeleton>
 
-                  <b-skeleton type="button"></b-skeleton>
+                <b-skeleton type="button"></b-skeleton>
               </b-card-body>
             </b-card>
           </b-col>
-         </b-row>
+        </b-row>
       </div>
 
     </template>
@@ -59,50 +68,58 @@
   import Main from './Main.vue';
   import axios from 'axios';
   export default {
-    components : {
+    components: {
       Main
     },
-    data(){
+    data() {
       return {
-        products : [],
-        loading : true
+        products: [],
+        loading: true
       }
     },
-    methods : {
-      addtocart(qty,price,size,color,product){
+    methods: {
+      addtocart(qty, price, size, color, product) {
 
         let cart = JSON.parse(localStorage.getItem('cart_storage'));
 
         cart = cart != null ? cart : [];
 
+        let index = cart.findIndex(c => c.product == product);
+
+        if (index !== -1) {
+          cart.splice(index, 1);
+        }
+
         cart.push({
-            qty       : qty,
-            price     : price,
-            size      : size,
-            color     : color,
-            product   : product
+
+          qty     : qty,
+          price   : price,
+          size    : size,
+          color   : color,
+          product : product
+
         });
 
-        localStorage.setItem('cart_storage',JSON.stringify(cart));
+        localStorage.setItem('cart_storage', JSON.stringify(cart));
 
       }
     },
-    created(){
-          axios.get('https://fakestoreapi.com/products')
-          .then(res => {
-            this.products = res.data;
-            this.loading = false;
-          })
-          .catch(err => {
-            console.log(err);
-          })
+    created() {
+      axios.get('https://fakestoreapi.com/products')
+        .then(res => {
+          this.products = res.data;
+          this.loading = false;
+        })
+        .catch(err => {
+          console.log(err);
+        })
     }
   }
 </script>
 
 <style>
-  .card-img-custom{
-    width : 200px;
+  .card-img-custom {
+    width: 200px;
     align-items: center;
     position: relative;
     left: 100px;
