b6a04591354b983a4bfc61014150826e38229984
nirav-bacancy <nirav.mistri@bacancy.com>
2022-02-02 10:09:22 +0530

    changes related to filter

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,47 +5,87 @@
     </b-row>
 
     <b-row>
-      <b-col sm="3" class="mt-1" v-for="product in products" :key="product.id">
-        <b-card
-          :img-src="product.image"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem"
-          class="mb-2"
-        >
-          <h5>
-            {{ product.title }}
-            <b-link
-              :class="'float-end ' + product.id"
-              @click="addProductWishlist(product.id)"
-            >
-              <!--<img src="../assets/heart-fill.svg" />-->
-              <img
-                v-if="product.isFavorite == 1"
-                src="../assets/heart-fill.svg"
-              />
-              <img v-else src="../assets/heart.svg" />
-            </b-link>
-          </h5>
+      <b-col cols="6" md="2">
+        <b-row>
+          <b-col sm="12" class="my-2">
+            <b-form-group id="brand" label="Brand:">
+              <b-form-select
+                class="form-select form-select-sm"
+                id="brand"
+                :options="brandOptions"
+                required
+                size="sm"
+                name="brand"
+                v-model="brand"
+              ></b-form-select>
+            </b-form-group>
+            <hr />
+            <b-form-group label="Color" v-slot="{ ariaDescribedby }">
+              <b-form-checkbox-group
+                id="color"
+                v-model="color"
+                :options="colorOptions"
+                value-field="value"
+                text-field="text"
+                :aria-describedby="ariaDescribedby"
+                name="color"
+              >
+              </b-form-checkbox-group>
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-col>
 
-          <b-card-text>
-            Price:
-            <span class="text-danger">${{ product.price }}</span>
-          </b-card-text>
-          <router-link
-            class="btn btn-outline-primary"
-            :to="'/product/' + product.id"
-            >View Details</router-link
+      <b-col cols="12" md="10">
+        <b-row>
+          <b-col
+            sm="3"
+            class="mt-4"
+            v-for="product in computedProducts"
+            :key="product.id"
           >
-        </b-card>
+            <b-card
+              :img-src="product.image"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+            >
+              <h5>
+                {{ product.title }}
+                <b-link
+                  :class="'float-end ' + product.id"
+                  @click="addProductWishlist(product.id)"
+                >
+                  <!--<img src="../assets/heart-fill.svg" />-->
+                  <img
+                    v-if="product.isFavorite == 1"
+                    src="../assets/heart-fill.svg"
+                  />
+                  <img v-else src="../assets/heart.svg" />
+                </b-link>
+              </h5>
+
+              <b-card-text>
+                Price:
+                <span class="text-danger">${{ product.price }}</span>
+              </b-card-text>
+              <router-link
+                class="btn btn-outline-primary"
+                :to="'/product/' + product.id"
+                >View Details</router-link
+              >
+            </b-card>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </b-container>
   <b-container v-else>
     <b-row>
       <b-col> Please Login first </b-col>
-    </b-row>    
+    </b-row>
   </b-container>
 </template>
 <script>
@@ -57,15 +97,29 @@ export default {
       products,
       favoriteProducts: [],
       already_login: false,
+      brand: null,
+      brandOptions: [
+        { value: null, text: "Select Brand" },
+        { value: "Nokia", text: "Nokia" },
+        { value: "Samsung", text: "Samsung" },
+        { value: "Motorola", text: "Motorola" },
+        { value: "Redmi", text: "Redmi" },
+      ],
+      color: [],
+      colorOptions: [
+        { text: "Red", value: "red" },
+        { text: "Black", value: "black" },
+        { text: "Yellow", value: "yellow" },
+      ],
     };
   },
   created() {
     var localIsLogin = JSON.parse(localStorage.getItem("is_login"));
     if (localIsLogin == null) localIsLogin = [];
-    console.log("is login val"+localIsLogin);
     if (localIsLogin == true) {
       this.already_login = true;
     }
+    this.allProducts = this.products;
   },
   mounted() {
     var localWishListProducts = JSON.parse(
@@ -81,6 +135,18 @@ export default {
       });
     }
     console.log(this.favItem);
+  },
+  computed: {
+    computedProducts: function () {
+      return this.products.filter((item) => {
+        return (
+          (this.brand === null ||
+            this.brand.length === 0 ||
+            item.brand.includes(this.brand)) &&
+          (this.color.length === 0 || item.color.includes(this.color))
+        );
+      });
+    },
   },
   methods: {
     addProductWishlist(productId) {
@@ -119,18 +185,6 @@ export default {
           JSON.stringify(localWishListProducts)
         );
       }
-    },
-    IsProductInWishList(productId) {
-      var localWishListProducts = JSON.parse(
-        localStorage.getItem("wish-products")
-      );
-      if (localWishListProducts) {
-        var ab = localWishListProducts.map(function (a) {
-          return a.id;
-        });
-        console.log(productId, ab);
-      }
-      return true;
     },
   },
 };
--- src/data/Product.js
+++ src/data/Product.js
@@ -8,7 +8,9 @@ export const products = [
       "To survive family life, you need energy. Lots of energy. Luckily, Nokia G10 has what you need, with a battery that goes for days – not hours – between charges. The big screen will captivate your kids for the whole of the journey, and its triple rear camera with enhanced imagery means you’re always ready to catch those special moments, whenever they might happen. Combine that with the latest Android™ 11, two years of software upgrades and three years of security updates and you’ve got a phone that will truly stand the test of time.",
     price: 100,
 	otherData:"Nokia G10, 6.5” HD+ Screen, 5050 mAh Battery, Triple Camera, 4 + 64GB Memory",
-  isFavorite:0
+  isFavorite:0,
+  color:'red',
+  brand:'Nokia',
   },
   {
     id: 2,
@@ -19,7 +21,9 @@ export const products = [
       "2.3GHz Qualcomm Snapdragon 720G with 8nm octa core processor,48MP rear camera with 8MP ultra-wide, 2MP super macro, 2MP portrait camera. Night mode, 960fps slowmotion, AI scene recognition, pro color, HDR, pro mode. 16MP front facing camera,16.94 centimeters (6.67-inch) FHD+ full screen dot display LCD multi-touch capacitive touchscreen with 2400 x 1080 pixels resolution,5020mAh lithium-polymer large battery providing talk-time of 29 hours and standby time of 556 hours | 18W fast charger in-box, Memory, Storage & SIM: 6GB RAM | 128GB storage | Dual SIM (nano+nano) + Dedicated SD card slot",
     price: 200,
 	otherData:"Redmi Note 10 Lite Glacier White 6GB RAM 128GB ROM | Alexa Built-in",
-  isFavorite:0
+  isFavorite:0,
+  color:'red',
+  brand:'Redmi'
   },
   {
     id: 3,
@@ -30,7 +34,9 @@ export const products = [
       "6 GB RAM | 128 GB ROM, Stock Android Experience,17.22 cm (6.78 inch) Full HD+ Display,108MP + 8MP + 2MP | 32MP Front Camera,6000 mAh Battery, 120Hz Refresh Rate",
     price: 300,
 	otherData:"MOTOROLA G60 (Dynamic Gray, 6GB RAM, 128GB Storage), 6000 mAh Battery, Triple Camera",
-  isFavorite:0
+  isFavorite:0,
+  color:'black',
+  brand:'Motorola'
   },
   {
     id: 4,
@@ -41,6 +47,8 @@ export const products = [
       "MediaTek | Dimensity 720 Octa Core 2GHz Processor with the widest 12 band support for a True 5G experience,16.55 centimeters (6.5-inch) TFT - Infinity V-cut display, HD+ resolution, 720 X 1600 pixels protected by Gorilla Glass 5,Versatile Quad camera setup-48MP (F 1.8) main camera + 8MP (F2.2) Ultra wide camera+ 5MP (F2.4) depth camera + 2MP (2.4) Macro Camera| 13MP (F2.2) front camera,Monster 5000 mAh Battery | Memory, Storage & SIM: Upto 12GB RAM Plus| 128GB internal memory expandable up to 1TB| SIM 1 + Hybrid (SIM or MicroSD),Latest Android v11.0, One UI 3.1 operating system, available right out of the box",
     price: 400,
 	otherData:"Samsung Galaxy M32 5G (Sky Blue, 8GB RAM, 128GB Storage)",
-  isFavorite:0
+  isFavorite:0,
+  color:'yellow',
+  brand:'Samsung'
   },
 ];
