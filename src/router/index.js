1d3d7d0022c6274976accaa3387fd3cbf34eba8d
Kalpana Jasoliya <kalpana.jasoliya@bacancy.com>
2022-02-09 09:08:10 +0530

    Login With Google/Facebook Features work on 9th Feb 2021

--- src/router/index.js
+++ src/router/index.js
@@ -2,61 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from '../store/index'
 
-Vue.use(VueRouter)
-
 const Login = () => import('@/views/auth/Login.vue')
 const Register = () => import('@/views/auth/Register.vue')
 const Dashboard = () => import('@/views/products/ProductsList.vue')
 const ProductDetails = () => import('@/views/products/ProductDetail.vue')
 const Checkout = () => import('@/views/products/Checkout.vue')
 
-router.beforeEach((to, from, next) => {
-  let roles = localStorage.getItem('roles')
-  let isLoggedIn = store.state.auth.isLogin
-  if (roles != null) {
-    roles = roles.split(',')
-  }
-  if (to.matched.some((record) => record.meta.requiresAuthLogin)) {
-    if (isLoggedIn) {
-      next()
-    } else {
-      next({
-        path: '/',
-        params: { nextUrl: to.fullPath },
-      })
-    }
-  }
-  if (to.matched.some((record) => record.meta.requiredBeforeLoginAccess)) {
-    if (!isLoggedIn) {
-      next()
-    } else {
-      next({
-        path: '/dashboard',
-        params: { nextUrl: to.fullPath },
-      })
-    }
-  } else if (to.matched.some((record) => record.meta.requiresAdmin)) {
-    if (roles != null && roles.indexOf('admin') >= 0) {
-      next()
-    } else {
-      next({
-        path: '/',
-        params: { nextUrl: to.fullPath },
-      })
-    }
-  } else if (to.matched.some((record) => record.meta.requiresUser)) {
-    if (roles != null && roles.indexOf('user') >= 0) {
-      next()
-    } else {
-      next({
-        path: '/',
-        params: { nextUrl: to.fullPath },
-      })
-    }
-  } else {
-    next()
-  }
-})
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -105,6 +57,54 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  let roles = localStorage.getItem('roles')
+  let isLoggedIn = store.state.auth.isLogin
+  if (roles != null) {
+    roles = roles.split(',')
+  }
+  if (to.matched.some((record) => record.meta.requiresAuthLogin)) {
+    if (isLoggedIn) {
+      next()
+    } else {
+      next({
+        path: '/',
+        params: { nextUrl: to.fullPath },
+      })
+    }
+  }
+  if (to.matched.some((record) => record.meta.requiredBeforeLoginAccess)) {
+    if (!isLoggedIn) {
+      next()
+    } else {
+      next({
+        path: '/dashboard',
+        params: { nextUrl: to.fullPath },
+      })
+    }
+  } else if (to.matched.some((record) => record.meta.requiresAdmin)) {
+    if (roles != null && roles.indexOf('admin') >= 0) {
+      next()
+    } else {
+      next({
+        path: '/',
+        params: { nextUrl: to.fullPath },
+      })
+    }
+  } else if (to.matched.some((record) => record.meta.requiresUser)) {
+    if (roles != null && roles.indexOf('user') >= 0) {
+      next()
+    } else {
+      next({
+        path: '/',
+        params: { nextUrl: to.fullPath },
+      })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
